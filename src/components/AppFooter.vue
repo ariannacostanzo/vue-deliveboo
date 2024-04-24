9232c238c290a22fe2a6b3bd75c37dcc82c4930e
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-04-24 17:40:38 +0200

    fixed responsive

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,39 +5,51 @@ export default {
 </script>
 
 <template>
-    <footer class="">
-        <div class="footer-content container-sm">
-            <div class="d-flex flex-column flex-md-row justify-content-between align-items-center">
-                <div class="logo mb-3 mb-md-0">
-                    <img src="../assets/logo-deliveboo.png" alt="Logo" width="100">
+    <footer class="py-4">
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-md-3 mb-3 mb-md-0">
+                    <div class="logo">
+                        <img src="../assets/logo-deliveboo.png" alt="Logo" class="img-fluid">
+                    </div>
                 </div>
-                <div class="social-icons mb-3 mb-md-0">
-                    <ul class="d-flex justify-content-center justify-content-md-start">
-                        <li><i class="fab fa-facebook"></i></li>
-                        <li><i class="fab fa-instagram"></i></li>
-                        <li><i class="fab fa-twitter"></i></li>
-                    </ul>
+                <div class="col-md-0 mb-3 mb-md-0">
+                    <div class="social-icons">
+                        <ul class="list-inline mb-0 d-flex justify-content-center justify-content-md-center pt-3">
+                            <li class="list-inline-item"><i class="fab fa-facebook"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-instagram"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-twitter"></i></li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="footer-links mb-3 mb-md-0">
-                    <ul class="d-flex justify-content-center justify-content-md-start">
-                        <li>FAQ</li>
-                        <li>Supporto</li>
-                        <li>Contatti</li>
-                        <li>Termini e condizioni</li>
-                    </ul>
+                <div class="col-md-0 mb-3 mb-md-0">
+                    <div class="footer-links">
+                        <ul class="list-unstyled mb-0 d-flex justify-content-center justify-content-md-center pt-3">
+                            <li><a href="#" class="text-decoration-none text-black">FAQ</a></li>
+                            <li><a href="#" class="text-decoration-none text-black">Supporto</a></li>
+                            <li><a href="#" class="text-decoration-none text-black">Contatti</a></li>
+                            <li><a href="#" class="text-decoration-none text-black">Termini e condizioni</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="store-icons mb-3 mb-md-0">
-                    <ul class="d-flex justify-content-center justify-content-md-start">
-                        <li><i class="fab fa-app-store"></i></li>
-                        <li><i class="fab fa-google-play"></i></li>
-                    </ul>
+                <div class="col-md-0 mb-3 mb-md-0">
+                    <div class="store-icons">
+                        <ul class="list-inline mb-0 d-flex justify-content-center justify-content-md-center py-3">
+                            <li class="list-inline-item"><i class="fab fa-app-store"></i></li>
+                            <li class="list-inline-item"><i class="fab fa-google-play"></i></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <div class="copyright text-center text-md-end">
-                © 2024 Deliveboo. Tutti i diritti riservati.
+            <div class="row">
+                <div class="col text-center text-md-end">
+                    <p class="mb-0">© 2024 Deliveboo. Tutti i diritti riservati.</p>
+                </div>
             </div>
         </div>
     </footer>
+
+
 </template>
 
 <style lang='scss'></style>
