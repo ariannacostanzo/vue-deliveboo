67210942539e93827bf50f74fcfbb71096f984a7
Amarildo <you@example.com>
2024-04-29 20:20:34 +0200

    added style to buttons and card effect

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -92,34 +92,40 @@ export default {
             </div>
         </div>
 
-        <!-- Filter Buttons -->
-        <div class="filter-container">
-            <button v-for="type in types" :key="type.id" @click="toggleFilter(type.label)"
-                :class="{ 'filter-button active': isFilterSelected(type.label), 'filter-button': !isFilterSelected(type.label) }">
-                <i :class="['fas', type.icon]"></i> {{ type.label }}
-                <span v-if="isFilterSelected(type.label)" @click.stop="toggleFilter(type.label)"
-                    class="close-filter">&times;</span>
-            </button>
-        </div>
 
-        <!-- Restaurant Cards -->
-        <section class="restaurant-container">
-            <div class="restaurant-card" v-for="(restaurant, index) in filteredRestaurants" :key="restaurant.id">
-                <img :src="restaurant.image" class="card-image" alt="Restaurant Image">
-                <div class="card-details">
-                    <h3 class="restaurant-name"><i class="fas fa-store"></i> {{ restaurant.name }}</h3>
-                    <p class="restaurant-address"><i class="fas fa-map-marker-alt"></i> {{ restaurant.address }}</p>
-                </div>
+        <div class="">
+            <!-- Filter Buttons -->
+            <div class="container filter-container">
+                <button v-for="type in types" :key="type.id" @click="toggleFilter(type.label)"
+                    :class="{ 'filter-button active': isFilterSelected(type.label), 'filter-button': !isFilterSelected(type.label) }">
+                    <i :class="['fas', type.icon]"></i> {{ type.label }}
+                    <span v-if="isFilterSelected(type.label)" @click.stop="toggleFilter(type.label)"
+                        class="close-filter">&times;</span>
+                </button>
             </div>
-        </section>
+
+
+            <!-- Restaurant Cards -->
+            <section class="restaurant-container">
+                <div class="restaurant-card" v-for="(restaurant, index) in filteredRestaurants" :key="restaurant.id">
+                    <div class="restourant-image">
+                        <img :src="restaurant.image" class="card-image" alt="Restaurant Image">
+                    </div>
+                    <div class="card-details content">
+                        <h3 class="restaurant-name"><i class="fas fa-store"></i> {{ restaurant.name }}</h3>
+                        <p class="restaurant-address"><i class="fas fa-map-marker-alt"></i> {{ restaurant.address }}</p>
+                    </div>
+                </div>
+            </section>
+        </div>
     </div>
 </template>
 
 <style lang='scss' scoped>
 // Search container
-.grower.animated {
+.grower.animated:hover {
     animation-name: grow;
-    animation-duration: 2s;
+    animation-duration: 1s;
     animation-iteration-count: infinite;
     animation-timing-function: linear;
 }
@@ -179,71 +185,118 @@ export default {
     transition: background-color 0.3s ease;
 }
 
-/* Effetto Hover sul pulsante di ricerca */
+// Effetto Hover sul pulsante di ricerca
 .search-button:hover {
     background-color: #e04a4e;
 }
 
-
-
-
-
 // Filter Button Styles
 .filter-container {
     margin-top: 20px;
+    gap: 0.5rem;
     display: flex;
     justify-content: center;
+    flex-wrap: wrap;
+    flex-direction: row;
 }
 
 .filter-button {
-    margin: 0 10px;
-    padding: 8px 15px;
+    display: flex;
+    margin: 0px 5px;
+    padding: 10px 25px;
     border: none;
-    border-radius: 20px;
+    border-radius: 25px;
     background-color: #f0f0f0;
     cursor: pointer;
-    transition: background-color 0.3s ease;
+    align-items: center;
+    justify-content: center;
+    transition: background-color 0.3s ease, transform 0.3s ease;
 }
 
-.filter-button.active {
-    background-color: #007bff;
-    color: white;
+.filter-button:hover {
+    transform: scale(1.1);
 }
 
 .close-filter {
+
+    display: flex;
+    background-color: white;
+
+    border-radius: 50%;
+
+    width: 25px;
+    height: 25px;
+
     cursor: pointer;
-    margin-left: 5px;
+
+    position: absolute;
+    top: 50%;
+    right: 0px;
+    transform: translateY(-2rem);
+
+    font-size: 1.5rem;
+    color: red;
+
+    transition: visibility 0.3s ease;
+    justify-content: center;
+    align-items: center;
 }
+
+.filter-button:hover .close-filter {
+    visibility: visible;
+}
+
+.filter-button.active {
+    background-color: #F48C06;
+    color: white;
+    position: relative;
+
+}
+
+.filter-button:hover .close-filter {
+    visibility: visible;
+}
+
 
 // Restaurant Card Styles 
-.restaurant-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin-top: 30px;
-}
-
 .restaurant-card {
-    width: 250px;
-    margin: 15px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    margin-top: 3rem;
+    margin-bottom: 3rem;
+
+    max-width: 300px;
+    background-color: #fff;
+    padding: 20px 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    box-shadow: 0 5px 20px rgba(0, 0, 0, 0.5);
+    transition: 0.3s ease-in-out;
     border-radius: 15px;
-    overflow: hidden;
-    transition: transform 0.3s ease;
+
+    position: relative;
 }
 
 .restaurant-card:hover {
-    transform: translateY(-5px);
+    transform: translateY(-20px);
 }
 
-.card-image {
+.restourant-image {
     width: 100%;
     height: 200px;
+    overflow: hidden;
+    border-radius: 15px;
+}
+
+.restourant-image img {
+    width: 100%;
+    height: 100%;
     object-fit: cover;
 }
 
 .card-details {
-    padding: 15px;
+    margin-top: 20px;
+    text-align: center;
 }
 
 .restaurant-name {
--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -3,42 +3,42 @@ import axios from 'axios';
 
 export default {
     name: 'Jumbotron',
-    data() {
-        return {
-            searchTerm: '',
-            restaurants: [],
-            types: []
-        };
-    },
-    methods: {
-        async searchRestaurants() {
-            if (this.searchTerm.trim() !== '') {
-                try {
-                    const response = await axios.get('http://localhost:8000/api/restaurants');
-                    this.restaurants = response.data.filter(restaurant =>
-                        restaurant.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-                        restaurant.types.some(type => type.label.toLowerCase().includes(this.searchTerm.toLowerCase()))
-                    );
-                } catch (error) {
-                    console.error(error);
-                }
-            } else {
-                // Se la barra di ricerca è vuota, imposta vuoto
-                this.restaurants = [];
-            }
-        },
-        async fetchTypes() {
-            try {
-                const response = await axios.get('http://localhost:8000/api/types');
-                this.types = response.data;
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    },
-    created() {
-        this.fetchTypes();
-    }
+    // data() {
+    //     return {
+    //         searchTerm: '',
+    //         restaurants: [],
+    //         types: []
+    //     };
+    // },
+    // methods: {
+    //     async searchRestaurants() {
+    //         if (this.searchTerm.trim() !== '') {
+    //             try {
+    //                 const response = await axios.get('http://localhost:8000/api/restaurants');
+    //                 this.restaurants = response.data.filter(restaurant =>
+    //                     restaurant.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+    //                     restaurant.types.some(type => type.label.toLowerCase().includes(this.searchTerm.toLowerCase()))
+    //                 );
+    //             } catch (error) {
+    //                 console.error(error);
+    //             }
+    //         } else {
+    //             // Se la barra di ricerca è vuota, imposta vuoto
+    //             this.restaurants = [];
+    //         }
+    //     },
+    //     async fetchTypes() {
+    //         try {
+    //             const response = await axios.get('http://localhost:8000/api/types');
+    //             this.types = response.data;
+    //         } catch (error) {
+    //             console.error(error);
+    //         }
+    //     }
+    // },
+    // created() {
+    //     this.fetchTypes();
+    // }
 }
 </script>
 
@@ -73,35 +73,35 @@ export default {
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.4);
 
 
-    // SearchBar
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+    // // SearchBar
+    // display: flex;
+    // flex-direction: column;
+    // justify-content: center;
 
-    .form-control {
-        max-width: 500px;
-        height: 60px;
+    // .form-control {
+    //     max-width: 500px;
+    //     height: 60px;
 
-    }
+    // }
 
-    .search-bar {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
+    // .search-bar {
+    //     display: flex;
+    //     align-items: center;
+    //     justify-content: center;
+    // }
 
-    .card-body {
-        min-width: 800px;
-        min-height: 300px;
+    // .card-body {
+    //     min-width: 800px;
+    //     min-height: 300px;
 
-    }
+    // }
 
-    .title-jumbotron {
-        color: #F48C08;
-    }
+    // .title-jumbotron {
+    //     color: #F48C08;
+    // }
 
-    li {
-        list-style: none;
-    }
+    // li {
+    //     list-style: none;
+    // }
 }
 </style>
