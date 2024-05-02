3bc813fc44d9aac7dfacd5b22fda4d6fdcf07047
g1ammi93 <giammariacarelli93@hotmail.it>
2024-05-02 15:17:55 +0100

    Edit Show Restaurant Guest

--- src/components/pages/RestaurantDetailPage.vue
+++ src/components/pages/RestaurantDetailPage.vue
@@ -1,6 +1,7 @@
 <script>
 import axios from 'axios';
 import Loader from '../Loader.vue';
+import { RouterLink } from 'vue-router';
 const defaultEndpoint = 'http://localhost:8000/api/restaurants/';
 
 export default {
@@ -27,24 +28,45 @@ export default {
 
 <template>
 
-    <h2 class="text-center mt-3">{{ restaurants.name }}</h2>
-    <address class="text-center"> {{ restaurants.address }}</address>
+    <section class="my-jumbotron">
+        <div class="text-container">
+            <h2 class="text-center mt-3">{{ restaurants.name }}</h2>
+            <address class="text-center"> {{ restaurants.address }}</address>
+        </div>
+        <img :src="restaurants.image" alt="">
+    </section>
 
-    <img v-if="restaurants.image" :src="restaurants.image" :alt="restaurants.name"
-        class="img-fluid mx-auto d-block mb-5" style="width: 500px">
-    <hr>
-    <h4 class="text-center mb-5">Scegli cosa ordinare</h4>
-    <div class="row cards-container">
-        <div v-if="restaurants.dishes" v-for="dish in restaurants.dishes" :key="dish.id" class="col-2">
-            <div class="my-card">
-                <div class="card-image">
-                    <img v-if="dish.image" :src="dish.image" :alt="dish.name">
-                </div>
-                <div class="card-content d-flex flex-column justify-content-between">
-                    <h5 class="card-title">{{ dish.name }}</h5>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <p class="card-text"> {{ dish.price }}€</p>
-                        <button class="btn btn-sm btn-primary">+</button>
+
+    <!-- <img v-if="restaurants.image" :src="restaurants.image" :alt="restaurants.name"
+        class="img-fluid mx-auto d-block mb-5" style="width: 500px"> -->
+    <h4 class="text-center  my-5">Scegli cosa ordinare</h4>
+    <div class="container mb-5">
+        <span class="orange">
+            <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);"
+                aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item text-decoration-underline">
+                        <RouterLink :to="{ name: 'home-page', }">Home</RouterLink>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">{{ restaurants.name }}</li>
+                </ol>
+            </nav>
+        </span>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div v-if="restaurants.dishes" v-for="dish in restaurants.dishes" :key="dish.id"
+                class="col-lg-2 col-md-4 col-sm-6">
+                <div class="my-card">
+                    <div class="card-image">
+                        <img v-if="dish.image" :src="dish.image" :alt="dish.name">
+                    </div>
+                    <div class="card-content d-flex flex-column justify-content-between">
+                        <h5 class="card-title">{{ dish.name }}</h5>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="card-text"> {{ dish.price }}€</p>
+                            <span class="plus-button"><i class="fa-solid fa-plus"></i></span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -53,22 +75,66 @@ export default {
 </template>
 
 <style lang='scss' scoped>
-// .restaurant-card {
-//     width: 333px;
-//     height: 333px;
-// }
+.orange {
+    color: #f48c06;
+}
+
+.my-jumbotron {
+    height: 300px;
+    opacity: 0.9;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.4);
+    position: relative;
+
+}
+
+.my-jumbotron img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    filter: grayscale(30%) brightness(70%) blur(3px);
+}
+
+h2 {
+    font-size: 50px;
+}
+
+address {
+    font-size: 20px;
+}
+
+.text-container {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    color: #f48c06;
+    text-shadow: 0px 0px 5px black;
+    z-index: 1;
+}
+
 
 .cards-container {
-    // display: flex;
-    // flex-wrap: wrap;
-    // justify-content: space-between;
     margin: auto;
     width: 1600px;
 }
 
+.plus-button {
+    padding: 5px;
+    background-color: #f48d06ad;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    cursor: pointer;
+
+}
+
 
 .my-card {
-    // width: calc(15% - 20px);
     margin-bottom: 30px;
     border: 1px solid #ccc;
     border-radius: 8px;
@@ -86,10 +152,6 @@ export default {
     transform: translateY(-20px);
 }
 
-// .card:hover {
-//     box-shadow: 0 6px 10px rgba(0, 0, 0, 0.2);
-// }
-
 .card-image img {
     width: 100%;
     height: 230px;
@@ -100,12 +162,6 @@ export default {
     padding: 10px;
     flex: 1;
 }
-
-// .card-title {
-//     margin-top: 0;
-//     margin-bottom: 10px;
-//     font-size: 1.2rem;
-// }
 
 .card-text {
     margin: 0;
